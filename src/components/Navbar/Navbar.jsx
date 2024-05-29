3667080a3cbfe565267a5be88af501fdfc1c3b1f
Adonis <adonis.casado@outlook.com>
2024-05-29 15:26:54 -0400

    changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/">Portfolio</a>
+      <a className={styles.title} href="/">adonis.dev</a>
       <div className={styles.menu}>
       <img className={styles.menuBtn} src={
         menuOpen 
